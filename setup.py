c36b86037d787588b7d7eb7e38b38cb48f9ef939
RanaSanga85 <rishabhbarman10@gmail.com>
2024-10-02 19:14:15 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "US_Visa",
+    version = "0.0.0",
+    author = "Rishabh",
+    author_email = "rishabhbarman10@gmail.com",
+    packages = find_packages()
+)
